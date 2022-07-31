856df21a5680a3d89700a607af8bbe0a5c62c158
Kai D. Gonzalez <gkai70264@gmail.com>
2022-07-31 00:20:44 -0400

    first commit

---
+++ index.mjs
@@ -0,0 +1,85 @@
+#!/usr/bin/node
+
+import { get } from 'https'; // or 'https' for https:// URLs
+import { existsSync, writeFileSync, readFileSync, fstat, writeFile } from 'fs';
+import { homedir } from "os";
+import decmprs from "decompress";
+import chalk from 'chalk';
+import { ArgumentParser } from 'argparse';
+import fetch from 'node-fetch'
+import { text as _text } from 'input';
+
+// const file = fs.createWriteStream("file.jpg");
+// const request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+//    response.pipe(file);
+
+//    // after download completed close filestream
+//    file.on("finish", () => {
+//        file.close();
+//        console.log("Download Completed");
+//    });
+// });
+
+
+async function question(text, default_value) {
+    const ans = await _text(text, { default: default_value });
+
+    return (ans)
+}
+
+
+
+const parser = new ArgumentParser({
+    description: 'POP is a package manager that aims to be a\nreplacement for KAP and DPKG On Kux.'
+});
+
+let sample_pop_config = {
+    "repos": {
+        "dax-stable": "https://raw.githubusercontent.com/thekaigonzalez/DaxRepo/stable",
+    }
+}
+
+const pkgcmds = parser.add_subparsers({ help: "Package Commands." })
+
+let action = pkgcmds.add_parser("install", {})
+
+action.add_argument("pkg", { help: "The package to (install)" })
+
+let args = parser.parse_args();
+let pop_conf = homedir() + "/.pop"
+
+if (!existsSync(pop_conf)) {
+    console.log("warning: config file does not exist")
+    console.log("creating basic file")
+    writeFileSync(pop_conf, JSON.stringify(sample_pop_config))
+}
+
+let popConf = JSON.parse(readFileSync(pop_conf));
+
+let pkg_exists = 0;
+
+console.log("resolving package...")
+
+Object.keys(popConf.repos).forEach(branch => {
+    fetch(popConf.repos[branch] + "/" + args.pkg, { method: "GET" })
+        .then(async (response) => {
+            console.log("hi")
+            let bin = await response.arrayBuffer()
+
+            if (response.status != 400) {
+                console.log(chalk.yellowBright("found package!"))
+                console.log("would you like to install the following package?\n\t" + chalk.blueBright(args.pkg) + "\n")
+                let yn = await question("Would you like to install the package?", "no")
+
+                if (yn == "yes") {
+                    console.log("creating file pipe...")
+                    bin = Buffer.from(bin)
+                    writeFileSync(args.pkg, bin)
+                }
+
+                pkg_exists = 1
+            }
+        }).catch((e) => {
+            console.log(e)
+        })
+})
